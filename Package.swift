5f1932fa668c0427dacd81564c19e9cf797f0206
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-02-20 19:42:38 +0900

    Add exlude to Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FNV",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["FNV.xcodeproj", "README.md", "Sources/FNV/Info.plist", "Sources/FNV/FNV.h"]),
         .testTarget(
             name: "FNVTests",
             dependencies: ["FNV"]),
