d15045aed8285cd6397879672819d2c18c3053b7
Daisuke TONOSAKI <daisuke.t.jp@gmail.com>
2020-09-22 16:27:01 +0900

    Change tab from 2 space to 4 space

--- Sources/FNV/Common.swift
+++ Sources/FNV/Common.swift
@@ -9,55 +9,55 @@
 import Foundation
 
 final class Common {
-  
-  // MARK: - Enum, Const
-  static let offsetBasis32: UInt32 = 2166136261
-  static let offsetBasis64: UInt64 = 14695981039346656037
-  static let prime32: UInt32 = 16777619
-  static let prime64: UInt64 = 1099511628211
-  
+    
+    // MARK: - Enum, Const
+    static let offsetBasis32: UInt32 = 2166136261
+    static let offsetBasis64: UInt64 = 14695981039346656037
+    static let prime32: UInt32 = 16777619
+    static let prime64: UInt64 = 1099511628211
+    
 }
 
 
 
 // MARK: - Utility
 extension Common {
-  
-  static func UInt32ToHex(_ val: UInt32) -> String {
-    return String.init(format: "%08x", val)
-  }
-  
-  static func UInt64ToHex(_ val: UInt64) -> String {
-    return String.init(format: "%016lx", val)
-  }
-  
+    
+    static func UInt32ToHex(_ val: UInt32) -> String {
+        return String.init(format: "%08x", val)
+    }
+    
+    static func UInt64ToHex(_ val: UInt64) -> String {
+        return String.init(format: "%016lx", val)
+    }
+    
 }
 
 
 
 // MARK: - Algorithm
 extension Common {
-  
-  static func fnv1<T: FixedWidthInteger>(_ array: [UInt8], offsetBasis: T, prime: T) -> T {
-    var hash: T = offsetBasis
     
-    for elm in array {
-      hash = hash &* prime
-      hash = hash ^ T(elm)
+    static func fnv1<T: FixedWidthInteger>(_ array: [UInt8], offsetBasis: T, prime: T) -> T {
+        var hash: T = offsetBasis
+        
+        for elm in array {
+            hash = hash &* prime
+            hash = hash ^ T(elm)
+        }
+        
+        return hash
     }
     
-    return hash
-  }
-  
-  static func fnv1a<T: FixedWidthInteger>(_ array: [UInt8], offsetBasis: T, prime: T) -> T {
-    var hash: T = offsetBasis
-    
-    for elm in array {
-      hash = hash ^ T(elm)
-      hash = hash &* prime
+    static func fnv1a<T: FixedWidthInteger>(_ array: [UInt8], offsetBasis: T, prime: T) -> T {
+        var hash: T = offsetBasis
+        
+        for elm in array {
+            hash = hash ^ T(elm)
+            hash = hash &* prime
+        }
+        
+        return hash
     }
     
-    return hash
-  }
-  
 }
--- Sources/FNV/FNV.swift
+++ Sources/FNV/FNV.swift
@@ -10,7 +10,7 @@ import Foundation
 
 /// FNV class
 public class FNV {
-  
+
 }
 
 
